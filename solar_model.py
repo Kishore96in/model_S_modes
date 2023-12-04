de005abc41e1731f11670bc7ab802a247638ecd0
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-12-04 12:35:32 +0530

    solar_model.py/read_extensive_model: a class to read the extensive solar model

--- solar_model.py
+++ solar_model.py
@@ -41,6 +41,75 @@ class read_limited_model(model_reader):
 		
 		self.sort_by(r)
 
+class read_extensive_model(model_reader):
+	"""
+	Read the extensive solar model in GONG format, downloaded from <https://users-phys.au.dk/~jcd/solar_models/>.
+	"""
+	gnames = {
+		#Indices of the global parameters in the file
+		'M_sun': 0,
+		'R_sun': 1,
+		}
+	
+	vnames = {
+		#Indices of the model variables at each mesh point
+		'rbyR': 0,
+		'T': 2,
+		'P': 3,
+		'rho': 4,
+		'Gamma_1': 9,
+		'delta': 11,
+		'CP': 12,
+		}
+	
+	def __init__(self, filename):
+		glob, var = self.read_extensive_solar_model(filename)
+		
+		for k in gnames.keys():
+			setattr(self, k, glob[gnames[k]])
+		for k in vnames.keys():
+			setattr(self, k, var[:,vnames[k]])
+		
+		self.r = self.rbyR * self.R_sun
+		self.G = 6.67408e-11 * 1e2**3 * 1e-3 #CGS
+		
+		self.CV = self.CP**2/( self.P*self.Gamma_1*self.delta**2/(self.rho*self.T) + self.CP )
+		
+		self.sort_by(self.r)
+	
+	def read_extensive_solar_model(filename):
+		"""
+		Format: specified at https://users-phys.au.dk/~jcd/solar_models/file-format.pdf
+			Record 1: Name of model (as a character string)
+			Record 2 – 4: Explanatory text, in free format
+			Record 5: nn, iconst, ivar, ivers
+			Record 6 – 8: glob(i), i = 1, ..., iconst
+			Record 9 – : var(i,n), i = 1, . . ., ivar, n = 1, . . . nn.
+		Download extensive data from https://users-phys.au.dk/~jcd/solar_models/fgong.l5bi.d.15
+		"""
+		def split_to_words(string, wordlen):
+			return [ string[i:wordlen+i] for i in range(0, len(string)-1,wordlen) ] #The -1 is because we expect a newline at the end of each string.
+		
+		with open(filename, 'r') as f:
+			lines = f.readlines()
+		model_name = lines[0]
+		model_description = ''.join(lines[1:4])
+		
+		nn, iconst, ivar, ivers = [ int(i) for i in lines[4].split() ]
+		
+		word_len = 16 #Each 'word' in line[5] onwards is 16 chars long.
+		ncols = len( split_to_words(lines[5],word_len) ) #Number of columns in record 6 onwards. Assume this is the same for all following records.
+		nlines_each_mesh = int(ivar/ncols) #The number of lines each mesh point occupies.
+		
+		glob = np.array( list( chain( *(split_to_words(i,word_len) for i in lines[5:8] ) ) ) , dtype=float)
+		
+		#We will construct var such that the first index refers to the mesh point, and the second index refers to the variable name.
+		var = []
+		for i in range(8,len(lines),nlines_each_mesh):
+			var.append( list( chain( *( split_to_words(i,word_len) for i in lines[i:i+nlines_each_mesh] ) ) ) )
+		
+		return glob, np.array(var, dtype=float)
+
 class solar_model():
 	def __init__(self, filename):
 		d = read_limited_model(filename)
