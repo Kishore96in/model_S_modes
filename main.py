530f7fcb661ca54487bbaffffd3f4a696b83fa41
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-12-04 09:18:36 +0530

    main.py/read_model_file: class to read solar model S

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ References:
 	BirKosDuv04: Birch, Kosovichev, Duvall 2004 - Sensitivity of Acoustic Wave Travel Times to Sound-Speed Perturbations in the Solar Interior
 """
 
+import numpy as np
+
+class read_model_file():
+	def __init__(self):
+		self.r, self.c, self.rho, self.P, self.gamma, self.T = np.loadtxt('solar_model_S_cptrho.l5bi.d.15c', unpack=True)
+
