ae151b4863b820d7f6809dde7063d41a7ebae465
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-12-04 09:21:37 +0530

    main.py/{rhs,bc}: RHS and BC of the boundary value problem

--- main.py
+++ main.py
@@ -38,3 +38,58 @@ class solar_model():
 		"""
 		return scipy.interpolate.UnivariateSpline(x, y, check_finite=True, ext='raise')
 
+def rhs(z, y, p, k, model):
+	"""
+	Equations A10 and A1 of [BirKosDuv04].
+	Note that the scale heights etc are themselves functions of depth determined by Model S.
+	
+	Arguments:
+		z, y, p: see scipy.integrate.solve_bvp
+		k: horizontal wavenumber
+		model: instance of solar_model
+	"""
+	dydz = np.zeros_like(y)
+	
+	y1, y2 = y
+	assert len(p) == 1
+	omega = p[0]
+	
+	c = model.c(z)
+	H = model.H(z)
+	N2 = model.N2(z)
+	
+	dydz[0] = (-1/c)*(
+		- (c/H)*y1
+		+ (1/omega)*(omega**2/Gamma - N2)*y2
+		)
+	
+	dydz[1] = (-1/c)*(
+		- (1/omega)*(omega**2 - Gamma*c**2*k**2)*y1
+		+ (c/H)*y2
+		)
+	
+	return dydz
+
+def bc(y_bot, y_top, p, k, model):
+	"""
+	Equations A20 and A21 of [BirKosDuv04].
+	
+	Arguments:
+		y_bot, y_top, p: see scipy.integrate.solve_bvp
+		k: horizontal wavenumber
+		model: instance of solar_model
+	"""
+	
+	y1,y2 = y
+	assert len(p) == 1
+	omega = p[0]
+	
+	c = model.c(z)
+	g = model.g(z)
+	
+	return np.array([
+		y2,
+		omega*y1 + (g/c)*y2,
+		*np.zeros_like(p),
+		])
+
