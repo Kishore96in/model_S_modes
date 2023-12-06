30241d87271736350b59bd17fe82a3a0f236fd48
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-12-06 10:31:42 +0530

    background_from_simulation.py: get variables from a Pencil simulation and construct a solar model from it

---
+++ background_from_simulation.py
@@ -0,0 +1,57 @@
+"""
+Generate a background model by reading simulation data (horizontal averages)
+
+"""
+
+import numpy as np
+
+from solar_model import solar_model
+
+class solar_model_from_sim(solar_model):
+	def __init__(self, filename):
+		with open(filename, 'rb') as f:
+			d = pickle.load(f)
+		
+		z = d['z']
+		g = d['gravz']
+		#NOTE: we assume the simulation uses an ideal gas EOS.
+		c = np.sqrt(d['cp']*(d['gamma']-1)*d['TTmz'])
+		H = 1/( - np.gradient(np.log(d['rhomz']), z)/2 - np.gradient(np.log(c), z)/2 - g/c**2 )
+		
+		grad_entropy = np.gradient(d['ssmz'], z)
+		N2 = - g*grad_entropy
+		
+		self.c = self.make_spline(z, c)
+		self.H = self.make_spline(z, H)
+		self.N2 = self.make_spline(z, N2)
+		self.g = self.make_spline(z, np.full_like(z, g))
+		
+		self.z_max = max(z)
+		self.z_min = min(z)
+
+def get_av(av, key, it=-500):
+	"""
+	Used to take the time average of a horizontal average.
+	"""
+	return np.average(getattr(av.xy, key)[it:], axis=0)
+
+if __name__ == "__main__":
+	import pencil as pc
+	import pickle
+	
+	param = pc.read.param(quiet=True)
+	grid = pc.read.grid(quiet=True, trim=True)
+	av = pc.read.aver(plane_list=['xy'], quiet=True)
+	
+	ret = {
+		'z': grid.z - param.zcool, #Make sure z=0 corresponds to the 'surface'.
+		'gravz': param.gravz,
+		'cp': param.cp,
+		'gamma': param.gamma,
+		'TTmz': get_av(av, 'TTmz'),
+		'rhomz': get_av(av, 'rhomz'),
+		'ssmz': get_av(av, 'ssmz'),
+		}
+	
+	with open("background.pickle", 'wb') as f:
+		pickle.dump(ret, f)
