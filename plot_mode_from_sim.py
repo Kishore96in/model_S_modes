c97ada27ef5920f957d529c4b0a3dcfc8e84ce4b
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-12-06 15:07:03 +0530

    plot_mode_from_sim.py: improve initial guesses

--- plot_mode_from_sim.py
+++ plot_mode_from_sim.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import scipy.integrate
 
 from bg_from_sim import solar_model_from_sim as solar_model
-from problem import rhs, bc_imp_both as bc, count_zero_crossings
+from problem import rhs, bc_imp_both as bc, count_zero_crossings, make_guess_pmode
 
 if __name__ == "__main__":
 	model = solar_model("background_a6.0l.1.pickle")
@@ -18,20 +18,12 @@ if __name__ == "__main__":
 	L_0 = model.c(0)**2/np.abs(model.g(0))
 	omega_0 = np.abs(model.g(0))/model.c(0)
 	
-	k = 0.1/L_0
+	k = 0/L_0
 	omega_guess = 0.5*omega_0
+	n_guess = 0 #number of nodes in the trial eigenmode used.
 	
-	z_guess = np.linspace(z_bot, z_top, 10)
-	
-	y_guess = np.array(
-		[
-			np.zeros_like(z_guess),
-			np.where(np.abs(z_guess + 0.2) < 0.2, 1, 0),
-			np.linspace(0,1,len(z_guess))
-			],
-		dtype=complex,
-		)
-	
+	z_guess = np.linspace(z_bot, z_top, 10+2*n_guess)
+	y_guess = make_guess_pmode(z_guess, n=n_guess)
 	p_guess = np.array([omega_guess])
 	
 	RHS = lambda z, y, p: rhs(z, y, p, k=k, model=model)
