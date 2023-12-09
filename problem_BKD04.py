problem_BKD04.py/bc: fix sign of g in boundary condition
Looks like those guys took g>0, so I need to flip the sign to match my
equations. Ugh.

problem_BKD04.py
@@ -73,7 +73,7 @@ def bc(y_bot, y_top, p, k, model, z_bot, z_top):
 	
 	return np.array([
 		y2_bot,
-		omega*y1_top + (g_top/c_top)*y2_top,
+		omega*y1_top - (g_top/c_top)*y2_top,
 		y3_bot,
 		y3_top - np.sign(np.real(omega)),
 		])