c1150352a922e5fef5e5a5744bab780efe794518
Kishore Gopalakrishnan <kishore96@gmail.com>
2023-12-23 13:07:43 +0530

    utils.add_arrow: function to add an arrow to a line plot.
    Copied directly from StackExchange

--- utils.py
+++ utils.py
@@ -71,3 +71,36 @@ def ralign_legend(l):
 	for t, shift in zip(texts, shifts):
 		t.set_ha('right')
 		t.set_position((shift,0))
+
+def add_arrow(line, position=None, direction='right', size=15, color=None):
+	"""
+	Add an arrow to a line. Copied from https://stackoverflow.com/questions/34017866/arrow-on-a-line-plot/34018322#34018322
+	
+	Arguments:
+		line:       Line2D object
+		position:   x-position of the arrow. If None, mean of xdata is taken
+		direction:  'left' or 'right'
+		size:       size of the arrow in fontsize points
+		color:      if None, line color is taken.
+	"""
+	if color is None:
+		color = line.get_color()
+
+	xdata = line.get_xdata()
+	ydata = line.get_ydata()
+
+	if position is None:
+		position = xdata.mean()
+	# find closest index
+	start_ind = np.argmin(np.absolute(xdata - position))
+	if direction == 'right':
+		end_ind = start_ind + 1
+	else:
+		end_ind = start_ind - 1
+
+	line.axes.annotate('',
+		xytext=(xdata[start_ind], ydata[start_ind]),
+		xy=(xdata[end_ind], ydata[end_ind]),
+		arrowprops=dict(arrowstyle="->", color=color),
+		size=size
+	)
