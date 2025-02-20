c723070dd3b199fe748e75cb54110bd2a695fe10
Kishore Gopalakrishnan <kishore96@gmail.com>
2025-02-20 13:01:13 +0530

    ADIPLS: plot_truncplane:: use 'correct' expression for k
    This is how l is defined in ADIPLS in plane parallel mode.

--- from_ADIPLS/plot_truncplane.py
+++ from_ADIPLS/plot_truncplane.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 	#Theoretical frequency of the f mode
 	ell = np.linspace(0, ax.get_xlim()[1], 1000)
 	R_sun = abs(model.z_min) #Assume the given model extends to the center of the Sun
-	k = np.sqrt(ell*(ell+1))/R_sun
+	k = ell/R_sun #equation A7 of adiab.prg.v0_3.pdf
 	g = abs(model.g(0))
 	ax.autoscale(False)
 	ax.plot(ell, 1e3*np.sqrt(g*k), ls='-', c='k')
@@ -40,7 +40,7 @@ if __name__ == "__main__":
 	ax.plot(ell, 1e3*max(model.c(z))*k, ls='--', c='k')
 	
 	ax.set_ylabel(r"$\omega$ (mHz)")
-	ax.set_xlabel(r"$\ell$")
+	ax.set_xlabel(r"$k R_\odot$")
 	ax.legend(loc='lower right', title="Nodes")
 	
 	save(fig, "komega_trunc_plane_cow.pdf")
